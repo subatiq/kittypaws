02070fd3f5168fdf04caea64108ae9e219c736f5
Vladimir Semenov <sepsseven@gmail.com>
2022-12-17 10:46:35 +0300

    Remove plugins from repo

--- pluginterface/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use std::collections::HashMap;
-
-pub type PlugConfig = HashMap<String, String>;
-
-pub trait PlugInterface {
-    fn run(&self, config: &PlugConfig);
-    fn clone_dyn(&self) -> Box<dyn PlugInterface + Send>;
-}
-
