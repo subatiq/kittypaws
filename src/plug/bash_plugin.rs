3906355fa73fb02ade03c167a7ad9eb657a892f3
Ilya Siluyanov <ilya.siluyanov@edgevision.pro>
2023-02-12 19:50:31 +0300

    Remove redundant; format code

--- src/plug/bash_plugin.rs
+++ src/plug/bash_plugin.rs
@@ -1,10 +1,9 @@
-use std::process::{Command, Child, Stdio, ChildStdout};
-use std::path::{Path,PathBuf};
-use std::collections::HashMap;
-use crate::plug::{unwrap_home_path, PluginInterface, CallablePlugin, PLUGINS_PATH};
-use std::io::Read;
+use crate::plug::{unwrap_home_path, CallablePlugin, PluginInterface, PLUGINS_PATH};
 use chrono::Local;
-
+use std::collections::HashMap;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+use std::process::{Child, ChildStdout, Command, Stdio};
 
 struct BashCommand {
     executable: PathBuf,
@@ -31,36 +30,31 @@ fn log(line: &str) {
     println!("[{}] {}", Local::now().format("%Y-%m-%d %H:%M:%S"), line);
 }
 
-fn try_get_line(stdout: &mut ChildStdout, collected: &mut Vec<u8>) -> Option<String>{
+fn try_get_line(stdout: &mut ChildStdout, collected: &mut Vec<u8>) -> Option<String> {
     let bytebuff: &mut [u8; 1] = &mut [0; 1];
     match stdout.read_exact(bytebuff) {
         Ok(_) => {
             collected.push(bytebuff[0]);
-        },
+        }
         Err(_) => {
             // println!("Error: {}", e);
         }
     }
 
-    match detect_line(&collected) {
-        Some(string) => {
-            return Some(string);
-        }
-        None => None,
-    }
+    detect_line(&collected)
 }
 
 impl PluginInterface for BashCommand {
     fn run(&self, config: &HashMap<String, String>) -> Result<(), String> {
         // get a string of args and values from hashmap
         let mut child = Command::new("bash")
-             .envs(config)
-             .arg("-C")
-             .arg(self.executable.to_str().unwrap())
-             .stdout(Stdio::piped())
-             // .args(&args)
-             .spawn()
-             .expect("failed to start process {}");
+            .envs(config)
+            .arg("-C")
+            .arg(self.executable.to_str().unwrap())
+            .stdout(Stdio::piped())
+            // .args(&args)
+            .spawn()
+            .expect("failed to start process {}");
         let mut stdout = child.stdout.take().expect("Stdout is available");
 
         let mut status;
@@ -82,15 +76,18 @@ impl PluginInterface for BashCommand {
         let status = status.unwrap();
         let result: Result<String, String> = match status {
             Ok(code) => {
-                let msg = format!("{} command exited with code {}",self.executable.to_str().unwrap(), code);
+                let msg = format!(
+                    "{} command exited with code {}",
+                    self.executable.to_str().unwrap(),
+                    code
+                );
                 match code {
                     0 => Ok(msg),
-                    _ => Err(msg)
+                    _ => Err(msg),
                 }
-            },
+            }
             Err(msg) => Err(msg),
         };
-
 
         // print non-error result
         // return error message
@@ -104,14 +101,17 @@ impl PluginInterface for BashCommand {
 
 pub fn load(name: &str) -> Result<CallablePlugin, String> {
     let plugins_path = unwrap_home_path(PLUGINS_PATH);
-    let plugins_dirname = plugins_path.to_str().expect("Can't find home directory for the current user");
+    let plugins_dirname = plugins_path
+        .to_str()
+        .expect("Can't find home directory for the current user");
 
     let entrypoint_path = format!("{}/{}/run.sh", &plugins_dirname, name);
     let path_to_main = Path::new(&entrypoint_path);
 
     match path_to_main.exists() {
-        true => Ok(Box::new(BashCommand { executable : path_to_main.to_path_buf() })),
-        false => Err(format!("No main.py found for plugin: {}", name))
+        true => Ok(Box::new(BashCommand {
+            executable: path_to_main.to_path_buf(),
+        })),
+        false => Err(format!("No main.py found for plugin: {}", name)),
     }
 }
-
