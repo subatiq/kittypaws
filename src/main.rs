ec889b9b3fab1bdb845ddc1b29cda7bffa53f592
Kirill Ivanov <bragov4ik@hotmail.com>
2023-04-22 02:50:52 +0300

    bind addr from argv

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,14 @@ use plug::start_main_loop;
 
 
 fn main() {
-    let binding = "127.0.0.1:9185".parse().unwrap();
-    let exporter = prometheus_exporter::start(binding).unwrap();
-    // get config path from args
+    // parse args
     let config_path = std::env::args().nth(1).or(Some("config.yml".to_string())).unwrap();
+    let bind_string = std::env::args().nth(2).or(Some("127.0.0.1:9185".to_string())).unwrap();
+
+    // init prometheus client
+    let binding = bind_string.as_str().parse().unwrap();
+    let _exporter = prometheus_exporter::start(binding).unwrap();
+
     let config = load_config(&config_path);
     start_main_loop(&config)
 }
